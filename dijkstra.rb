a3fc501cbe9a1cf87bd110d5e490ae5a7f80004e
Swathi Ravi <swathi.vasan7@gmail.com>
2016-02-06 20:08:55 +0530

    marked private methods

--- dijkstra.rb
+++ dijkstra.rb
@@ -5,37 +5,7 @@ class Dijkstra
   	@max= (matrix.flatten.max) + 999
   	@v=matrix.size
   end
-
-  def set_max_distance
-  	0.upto(@v-1) do |i|
-			0.upto(@v-1) do |j|
-				if @matrix[i][j] == 0
-					@matrix[i][j] = @max
-				end
-			end
-		end
-  end
-  
-  def calculate_min_distance_node(distance, visited)
-  	next_node = 0
-  	min = @max
-	  0.upto(@v-1) do |c|
-	  	if (visited[c] == false) && (min > distance[c])
-	  		min = distance[c]
-	  		next_node = c
-	  	end
-	  end
-	  return min, next_node
-  end
-
-  def set_default_values
-  	distance = @matrix[@src]  # initialize distance array to the source row from matrix
-  	distance[@src] = 0  # distance from source to source is 0
-  	visited = Array.new(@v) {|i| i=false }  # no nodes are visited
-  	visited[@src] = true # only the source row is marked visited
-  	return distance, visited
-  end
-  
+ 
   def dijkstra
   	set_max_distance # where no path is available set value to @max
   	distance, visited = set_default_values
@@ -56,18 +26,50 @@ class Dijkstra
     distance.each_with_index {|d,i| puts "Vertex #{i+1}   Distance  #{d}" }
     return distance
   end
+
+  private
+
+  def set_max_distance
+    0.upto(@v-1) do |i|
+      0.upto(@v-1) do |j|
+        if @matrix[i][j] == 0
+          @matrix[i][j] = @max
+        end
+      end
+    end
+  end
+  
+  def calculate_min_distance_node(distance, visited)
+    next_node = 0
+    min = @max
+    0.upto(@v-1) do |c|
+      if (visited[c] == false) && (min > distance[c])
+        min = distance[c]
+        next_node = c
+      end
+    end
+    return min, next_node
+  end
+
+  def set_default_values
+    distance = @matrix[@src]  # initialize distance array to the source row from matrix
+    distance[@src] = 0  # distance from source to source is 0
+    visited = Array.new(@v) {|i| i=false }  # no nodes are visited
+    visited[@src] = true # only the source row is marked visited
+    return distance, visited
+  end
 end
 
 # sample data from wikipedia
-# matrix = [
-#     [0, 7, 9, 0, 0, 14],
-#     [7, 0, 10, 15, 0, 0],
-#     [9, 10, 0, 11, 0, 2],
-#     [0, 15, 11, 0, 6, 0],
-#     [0, 0, 0, 6, 0, 9],
-#     [14, 0, 2, 0, 9, 0]
-#     ]
+matrix = [
+    [0, 7, 9, 0, 0, 14],
+    [7, 0, 10, 15, 0, 0],
+    [9, 10, 0, 11, 0, 2],
+    [0, 15, 11, 0, 6, 0],
+    [0, 0, 0, 6, 0, 9],
+    [14, 0, 2, 0, 9, 0]
+    ]
  
-# d  = Dijkstra.new(matrix, 1)
-# d.dijkstra
+d  = Dijkstra.new(matrix, 0)
+d.dijkstra
 # rspec --color --format documentation spec/dijkstra_spec.rb
